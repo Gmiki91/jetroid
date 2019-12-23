23c5c2153a3da9bc59035f521c28011853b82a72
Damokos Dóra <gmiki91@gmail.com>
2019-12-23 15:58:34 +0100

    init

---
+++ Assets/jetroid/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public Vector2 moving = new Vector2 ();
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		moving.x = 0;
+		moving.y = 0;
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			moving.x = 1;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			moving.x = -1;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			moving.y = -1;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			moving.y = 1;
+		}
+
+	}
+}
